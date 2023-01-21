5f583d3a1fc245551d29c5fa08945e49099f68ba
kdpone <pausangklyde@gmail.com>
2023-01-21 12:31:53 -0700

    implement 405

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -28,13 +29,56 @@ import socketserver
 
 # Klyde Pausang
 
+''' self.data returns:
+        b'GET HTTP/1.1\r\n
+        Host: 127.0.0.1:8080\r\n
+        User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0\r\n
+        Accept: image/avif,image/webp,*/*\r\n
+        Accept-Language: en-US,en;q=0.5\r\n
+        Accept-Encoding: gzip, deflate, br\r\n
+        Connection: keep-alive\r\n
+        Referer: http://127.0.0.1:8080/\r\n
+        Sec-Fetch-Dest: image\r\nSec-Fetch-Mode: no-cors\r\n
+        Sec-Fetch-Site: same-origin'''
+
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
-        self.data = self.request.recv(1024).strip()
+        self.data = self.request.recv(1024).strip().decode('utf-8')
         print ("Got a request of: %s\n" % self.data)
         self.request.sendall(bytearray("OK",'utf-8'))
+
+        #status code
+        self.status_code = 200
+        self.reason = ""
+
+
+        #Parse data and get request status
+        data_list = self.data.split()
+        request_status = data_list[0]
+
+        #path
+        path = os.path.abspath("www/index.html")
+
+        print(path)
+
+        
+
+      
+
+        
+
+        #Check if request_status is GET
+        if(request_status != 'GET'):
+            self.status_code = 405
+            self.reason = "Method Not Allowed"
+        else:
+            
+
+            pass
+
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
